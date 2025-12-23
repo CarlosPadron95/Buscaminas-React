ce9d79ad79a54fe9d839ac310aa9aee8e6e25b0d
CarlosPadron95 <carlospadrondelgado@gmail.com>
2025-12-23 16:12:08 +0000

    Corrigiendo ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/buscaminas/",
+  base: "/Buscaminas-React/",
   server: {
     watch: {
       usePolling: true,
